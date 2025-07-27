dc58a592d56f36a0cc4ad23fdfaac9c36189a787
Ludovic <dormoy.dev@gmail.com>
2025-07-27 22:09:55 +0200

    Add EmailController and email template
    Introduced EmailController.php and a corresponding Twig template for email functionality. Updated .gitignore to exclude .idea, and made minor adjustments to .env and messenger.yaml. Composer dependencies were updated to reflect new or changed packages.

---
+++ src/Controller/EmailController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class EmailController extends AbstractController
+{
+    /**
+     * @throws TransportExceptionInterface
+     */
+    #[Route('/email', name: 'app_email')]
+    public function index(MailerInterface $mailer): Response
+    {
+        $email = new Email();
+        $email->from('contact@contact.fr')
+            ->to('toto@gmail.com')
+            ->subject('Test email')
+            ->html('<h1>Test email</h1>');
+
+        $mailer->send($email);
+        return $this->render('email/index.html.twig', [
+            'controller_name' => 'EmailController',
+        ]);
+    }
+}
