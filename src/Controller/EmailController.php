f35af7e3f814daa4847ad950215f9cb6f9bda0f5
Ludovic <dormoy.dev@gmail.com>
2025-07-29 09:51:08 +0200

    Add welcome email template with inline CSS and image
    Introduced a new Twig email template (welcome.html.twig) using inline CSS and embedded image support. Updated EmailController to send templated emails with context, attachments, and improved recipient handling. Added related CSS and image assets, and configured Twig paths for public resources. Updated dependencies to include twig/cssinliner-extra for CSS inlining.

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -17,13 +19,18 @@ final class EmailController extends AbstractController
     #[Route('/email', name: 'app_email')]
     public function index(MailerInterface $mailer): Response
     {
-        $email = new Email();
+        $email = new TemplatedEmail();
         $email
-            ->to('toto@gmail.com')
+            ->to(new Address('toto@gmail.com', 'titi'))
             ->subject('Test email')
-            ->html('<h1>Test email</h1>');
+            ->htmlTemplate('email/welcome.html.twig')
+            ->context([
+                'username' => 'titi'
+            ])
+            ->attachFromPath('images/ex1.png', 'image1.png', 'image/png');
 
         $mailer->send($email);
+
         return $this->render('email/index.html.twig', [
             'controller_name' => 'EmailController',
         ]);
